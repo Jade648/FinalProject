4c7e5ad8201cf9399fd8b4017397575e9463f208
Jade 648 <maddiepeterson375@gmail.com>
2023-05-01 10:21:41 -0500

    Added Discounts page  and updated layout page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,9 +5,11 @@ public class ProductController : Controller
   // this controller depends on the NorthwindRepository
   private DataContext _dataContext;
   public ProductController(DataContext db) => _dataContext = db;
+
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   public IActionResult Index(int id){
     ViewBag.id = id;
     return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   }
+  public ActionResult Discount() => View(_dataContext.Discounts);
 }
