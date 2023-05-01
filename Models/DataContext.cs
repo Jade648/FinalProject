a67f6aa78f41bd54a921b6f4f5acd9ae43a1d4e2
Jade 648 <maddiepeterson375@gmail.com>
2023-04-30 22:57:44 -0500

    Updated Data Context file

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -10,7 +10,7 @@ public class DataContext : DbContext
   public DbSet<Customer> Customers { get; set; }
   public DbSet<CartItem> CartItems { get; set; }
 
-  public DbSet<Employee> employee {get; set;}
+  public DbSet<Employee> Employees {get; set;}
 
   public void AddCustomer(Customer customer)
   {
