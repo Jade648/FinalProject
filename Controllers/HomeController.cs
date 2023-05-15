b514e754b66f4bf28af722ae0aa062d974b87c08
Jade 648 <maddiepeterson375@gmail.com>
2023-05-15 08:18:49 -0500

    Changed Home Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,28 +7,9 @@ public class HomeController : Controller
     private DataContext _dataContext;
     public HomeController(DataContext db) => _dataContext = db;
     public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
-    public IActionResult AddDiscount() => View();
-
-    [HttpPost]
-
-public IActionResult AddDiscount(Discount model){
-
-{
-    if (ModelState.IsValid)
-    {
-      if (_dataContext.Discounts.Any(d => d.Title == model.Title))
-      {
-        ModelState.AddModelError("", "Title must be unique");
-      }
-      else
-      {
-        _dataContext.AddDiscount(model);
-        return RedirectToAction("Index");
-      }
-    }
-    return View();
+  
+ 
 }
-}
-}
+
 
 
