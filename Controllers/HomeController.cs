e5e0546af16a8e5a05985ea52642056129263329
Jade 648 <maddiepeterson375@gmail.com>
2023-05-15 08:20:26 -0500

    updated Home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,8 +7,7 @@ public class HomeController : Controller
     private DataContext _dataContext;
     public HomeController(DataContext db) => _dataContext = db;
     public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
-  
- 
+   
 }
 
 
